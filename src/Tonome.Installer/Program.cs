fbd1d14241826002cfd5a28f65c1fb4f0218ea52
eymenomersenyigit-lang <eymenomersenyigit@gmail.com>
2026-07-30 12:35:22 +0300

    Merge installer into tonome-desktop package, fix Program.cs to launch actual installer

--- src/Tonome.Installer/Program.cs
+++ src/Tonome.Installer/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tonome.Installer;
+
+var installer = new TonomeInstaller();
+installer.Run();
